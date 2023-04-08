3be9271edbf583098cfe51474091b362ab6b2906
EnlighterNo01 <tihomir.ilic.1991@gmail.com>
2023-04-08 17:51:06 +0200

    commit

--- src/data.js
+++ src/data.js
@@ -1,3 +1,9 @@
+import tour1 from './images/tour-1.jpeg'
+import tour2 from './images/tour-2.jpeg'
+import tour3 from './images/tour-3.jpeg'
+import tour4 from './images/tour-4.jpeg'
+
+
 export const pageLinks = [
     {id:1, href:'#home', text:'home'}, 
     {id:2, href:'#about', text:'about'}, 
@@ -15,4 +21,15 @@ export const services = [
     {id:1, icon:'fas fa-wallet fa-fw', title:'saving money', text:'Lorem ipsum dolor sit amet consectetur adipisicing elitAsperiores officia'},
     {id:2, icon:'fas fa-tree fa-fw', title:'endless hiking', text:'Lorem ipsum dolor sit amet consectetur adipisicing elitAsperiores officia'},
     {id:3, icon:'fas fa-socks fa-fw', title:'amazing comfort', text:'Lorem ipsum dolor sit amet consectetur adipisicing elitAsperiores officia'},
-]
+];
+
+export const tours = [
+    {id:1, image:tour1, date:'august 26th, 2020', title:'Tibet Adventure', info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location:'China', duration:6, cost: 2100},
+    {id:2, image:tour2, date:'august 30th, 2021', title:'Serbia Adventure', info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location:'Serbia', duration:6, cost: 2200},
+    {id:3, image:tour3, date:'august 28th, 2022', title:'Wakanda Adventure', info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location:'Wakanda Forever', duration:6, cost: 2100},
+    {id:4, image:tour4, date:'august 20th, 2023', title:'Brazil Spice', info:'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumquevitae tempore voluptatum maxime reprehenderit eum quod exercitationem fugit, qui corporis.',
+    location:'Brazil', duration:3, cost: 3100},
+];
