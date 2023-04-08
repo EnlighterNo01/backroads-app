7c8b035ace8f37cdf9c8b5c8ffb1b1b742e318ae
EnlighterNo01 <tihomir.ilic.1991@gmail.com>
2023-04-08 16:37:27 +0200

    social links?>

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import logo from '../images/logo.svg'
-import { pageLinks } from '../data';
+import { pageLinks, socialLinks } from '../data';
 
 const Navbar = () => {
   return (
@@ -22,35 +22,19 @@ const Navbar = () => {
                     </li>
                     );
                 })}
-                <li>
-                    <a href="#about" className="nav-link"> about </a>
-                </li>
-
-                <li>
-                    <a href="#services" className="nav-link"> services </a>
-                </li>
-
-                <li>
-                    <a href="#tours" className="nav-link"> tours</a>
-                </li>
             </ul>
 
             <ul className="nav-icons">
-                <li>
-                    <a href="https://www.twitter.com" target="_blank" className="nav-icon">
-                        <i className="fab fa-facebook"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.twitter.com" target="_blank" className="nav-icon">
-                        <i className="fab fa-twitter"></i>
-                    </a>
-                </li>
-                    <li>
-                    <a href="https://www.twitter.com" target="_blank" className="nav-icon">
-                        <i className="fab fa-squarespace"></i>
-                    </a>
-                </li>
+                {socialLinks.map ((link) => {
+                    const {id, href, icon} = link
+                    return (
+                        <li key={id}>
+                        <a href='' target="_blank" className="nav-icon">
+                            <i className={icon}></i>
+                        </a>
+                    </li>
+                    );
+                })}
             </ul>
         </div>
     </nav>
